5f25c43e8ea3d4709b7194df5409a86da2a57180
Charan Srinivas <144456036+Charanvas@users.noreply.github.com>
2025-10-12 12:10:14 +0530

    Update wsgi.py
    updating port configurations

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -6,8 +6,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from app import create_app
 
+# Create app instance
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
