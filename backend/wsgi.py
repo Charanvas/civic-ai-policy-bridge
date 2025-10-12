64419f59f4020348897a0992cc559cb057d93aa8
Charan Srinivas <144456036+Charanvas@users.noreply.github.com>
2025-10-12 09:02:28 +0530

    Update wsgi.py
    updated wsgi.py for deployment in railway.

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,11 @@
 from app import create_app
-import os
+from config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=Config.PORT,
+        debug=False
+    )
